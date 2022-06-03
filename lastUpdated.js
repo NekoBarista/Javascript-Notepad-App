89327e0a1d88cd422dc3e5fa9339a96e3b52a382
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-06-04 04:18:09 +1000

    fixing language to be clearer

--- lastUpdated.js
+++ lastUpdated.js
@@ -13,56 +13,56 @@ function lastUpdated (note) {
     if(timedifference >= 0 && timedifference <= 29000) {
         if(timedifference >= 0 && timedifference <= 60000) {
             let seconds = Math.round(timedifference/1000)
-            return `Last Updated  ${seconds} seconds ago`
+            return `Last updated  ${seconds} seconds ago`
     }
 }
 else if(timedifference >= 60001 && timedifference <= 3600000) {
-    if (minutes = 1) {
-        return `Last Updated ${minutes} minute ago`
+    if (minutes <= 1) {
+        return `Last updated ${minutes} minute ago`
         }
     
         else {
-            return `Last Updated ${minutes} minutes ago`
+            return `Last updated ${minutes} minutes ago`
             }
 }
 
 else if(timedifference >= 3600001 && timedifference <= 86400000) {
-if (hours = 1) {
-        return `Last Updated ${hours} hour ago`
+if (hours  <= 1) {
+        return `Last updated more than ${hours} hour ago`
         }
     
         else {
-            return `Last Updated ${hours} hours ago`
+            return `Last updated more than ${hours} hours ago`
             }
 }
 
 else if(timedifference >= 86400001 && timedifference <= 604800000) {
-    if (days = 1) {
-        return `Last Updated ${days} day ago`
+    if (days <= 1) {
+        return `Last updated more than ${days} day ago`
         }
     
         else {
-            return `Last Updated ${days} days ago`
+            return `Last updated more than ${days} days ago`
             }
 }
 
 else if(timedifference >= 604800000 && timedifference <= 2419200000) {
-    if (weeks = 1) {
-        return `Last Updated ${weeks} week ago`
+    if (weeks  <= 1) {
+        return `Last updated more than ${weeks} week ago`
         }
     
         else {
-            return `Last Updated ${weeks} weeks ago`
+            return `Last updated more than ${weeks} weeks ago`
             }
 }
 
 else if(timedifference >= 2419200001 && timedifference <= 29030400000) {
-    if (months = 1) {
-        return `Last Updated ${months} month ago`
+    if (months  <= 1) {
+        return `Last updated more than ${months} month ago`
         }
     
         else {
-            return `Last Updated ${months} months ago`
+            return `Last updated more than ${months} months ago`
             }
 }
 
